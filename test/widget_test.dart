1a2e58729ccebf39e59b1e85c310d577c843cfc7
Tyler Jewell <tyler.p.jewell@gmail.com>
2023-12-02 18:28:31 -0700

    Update app name and title
    Update app name and title in manifest.json, index.html, CMakeLists.txt,
    and my_application.cc files.
    
    Delete unused files and dependencies.
    
    Add integration test for counter increment.
    
    Update Flutter SDK version.
    
    Update pubspec.yaml and pubspec.lock files.
    
    Update .metadata file.
    
    Commit changes.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sample_app/main.dart';
+import 'package:mezops_flutter_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
